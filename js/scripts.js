d48ad7acefb4ecd5f8c22266cab2487d0221dfa8
jatinderapex <jatinder.singh@apexure.com>
2016-05-17 12:41:49 +0530

    fixing zoom

--- js/scripts.js
+++ js/scripts.js
@@ -24,17 +24,6 @@ if ($(this).scrollTop() > 1){
   }
 });
   
-function DisablePinchZoom() 
-{
-    $('meta[name=viewport]').attr("content", "");
-    $('meta[name=viewport]').attr("content", "width=yourwidth, user-scalable=no");
-}
-
-function myFunction() 
-{
-    $('meta[name=viewport]').attr("content", "width=1047, user-scalable=yes");
-}
-
 
   
 });
